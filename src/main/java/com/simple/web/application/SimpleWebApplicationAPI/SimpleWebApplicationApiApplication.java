d224a55d911b04f09dc569cd917f66654f8cca01
AntouelaBitsa <antoubi10@gmail.com>
2024-11-25 15:15:08 +0200

    Initial Commit - Project Upload

---
+++ src/main/java/com/simple/web/application/SimpleWebApplicationAPI/SimpleWebApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.simple.web.application.SimpleWebApplicationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/web/application/SimpleWebApplicationAPI/SimpleWebApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.web.application.SimpleWebApplicationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
