7311aa6efdab41279f3daf37b63c159618bf02d6
AntouelaBitsa <antoubi10@gmail.com>
2024-12-02 20:31:09 +0200

    Creating methods for Get All, Get By Id, Register Home address

--- src/main/java/com/simple/web/application/SimpleWebApplicationAPI/Service/HomeAddressService.java
+++ src/main/java/com/simple/web/application/SimpleWebApplicationAPI/Service/HomeAddressService.java
@@ -1,4 +1,39 @@
 package com.simple.web.application.SimpleWebApplicationAPI.Service;
 
+import com.simple.web.application.SimpleWebApplicationAPI.Entity.HomeAddress;
+import com.simple.web.application.SimpleWebApplicationAPI.Entity.WorkAddress;
+import com.simple.web.application.SimpleWebApplicationAPI.Repository.HomeAddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HomeAddressService {
+    @Autowired
+    private HomeAddressRepository homeAddressRepository;
+
+    public HomeAddress getHomeAddByID(Integer id){
+        HomeAddress homeAddress = new HomeAddress();
+        try {
+            homeAddress = homeAddressRepository.findById(id).orElse(null);
+        }catch (Exception e){
+            System.out.println("Exception Occurred on getWorkAddByID(): " + e);
+        }
+        return homeAddress;
+    }
+
+    public HomeAddress registerHomeAddress(String homeAddress){
+        System.out.println("[Debug01: registerWorkAddress()]: " + homeAddress.toString());
+        HomeAddress regHomeAdd = new HomeAddress();
+        regHomeAdd.setHomeAddress(homeAddress);
+
+        try {
+            regHomeAdd = homeAddressRepository.save(regHomeAdd);
+            System.out.println("[Debug02: WorkAddress saved]: " + regHomeAdd);
+        }catch (Exception e){
+            System.out.println("[Debug03: catch() exception]: " + regHomeAdd);
+            System.out.println("Exception Occurred on registerWorkAddress(): " + e.getMessage());
+        }
+
+        return regHomeAdd;
+    }
 }
--- src/main/java/com/simple/web/application/SimpleWebApplicationAPI/Service/WorkAddressService.java
+++ src/main/java/com/simple/web/application/SimpleWebApplicationAPI/Service/WorkAddressService.java
@@ -1,4 +1,51 @@
 package com.simple.web.application.SimpleWebApplicationAPI.Service;
 
+import com.simple.web.application.SimpleWebApplicationAPI.Entity.Users;
+import com.simple.web.application.SimpleWebApplicationAPI.Entity.WorkAddress;
+import com.simple.web.application.SimpleWebApplicationAPI.Repository.WorkAddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class WorkAddressService {
+    @Autowired
+    private WorkAddressRepository workAddressRepository;
+
+    public List<WorkAddress> getAllWorkAddresses() {
+        List<WorkAddress> workAddList = new ArrayList<>();
+        try {
+            workAddList = workAddressRepository.findAll();
+        }catch (Exception e){
+            System.out.println("Exception Occurred on getAllWorkAddresses(): " + e);
+        }
+        return workAddList;
+    }
+
+    public WorkAddress getWorkAddByID(Integer id){
+        WorkAddress workAddress = new WorkAddress();
+        try {
+            workAddress = workAddressRepository.findById(id).orElse(null);
+        }catch (Exception e){
+            System.out.println("Exception Occurred on getWorkAddByID(): " + e);
+        }
+        return workAddress;
+    }
+
+    public WorkAddress registerWorkAddress(String workAddress){
+        System.out.println("[Debug01: registerWorkAddress()]: " + workAddress.toString());
+        WorkAddress regWorkAdd = new WorkAddress();
+
+        try {
+            regWorkAdd = workAddressRepository.save(regWorkAdd);
+            System.out.println("[Debug02: WorkAddress saved]: " + regWorkAdd);
+        }catch (Exception e){
+            System.out.println("[Debug03: catch() exception]: " + regWorkAdd);
+            System.out.println("Exception Occurred on registerWorkAddress(): " + e.getMessage());
+        }
+
+        return regWorkAdd;
+    }
 }
