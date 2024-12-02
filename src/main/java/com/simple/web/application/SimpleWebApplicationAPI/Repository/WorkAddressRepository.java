576bfbffa8a1648caee9772969375338e73da120
AntouelaBitsa <antoubi10@gmail.com>
2024-12-02 20:29:42 +0200

    Adding Custom Query method
    to find a record based on work address

--- src/main/java/com/simple/web/application/SimpleWebApplicationAPI/Repository/WorkAddressRepository.java
+++ src/main/java/com/simple/web/application/SimpleWebApplicationAPI/Repository/WorkAddressRepository.java
@@ -4,4 +4,5 @@ import com.simple.web.application.SimpleWebApplicationAPI.Entity.WorkAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkAddressRepository extends JpaRepository<WorkAddress, Integer> {
+    WorkAddress findByWorkAddress(String workAddress);
 }
